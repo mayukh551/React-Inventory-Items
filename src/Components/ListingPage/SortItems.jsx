99791ab11b644d1e59d06fa37d6a5535527b5fb3
mayukh551 <may14ukh@gmail.com>
2022-08-14 20:02:57 +0530

    Sort bug removed

--- src/Components/ListingPage/SortItems.jsx
+++ src/Components/ListingPage/SortItems.jsx
@@ -8,23 +8,28 @@ const SortItems = (props) => {
 	// slice(3) to ignore the first 3 characters : 'R' , 's' and <space>
 
     if (sortOption === "Highest - Lowest") 
-	{
+	  {
       updatedList.sort((a, b) => {
-        if (b.price.slice(3) >= a.price.slice(3)) 
-			return 0;
+        console.log(a.price, b.price);
+        if (b.price.slice(3) > a.price.slice(3)) 
+			return 1;
 
         else if (b.price.slice(3) < a.price.slice(3)) 
 			return -1;
+
+      return 0;
       });
     } 
 	
 	else if (sortOption === "Lowest - Highest") {
       updatedList.sort((a, b) => {
-        if (b.price.slice(3) <= a.price.slice(3)) 
-			return 0;
+        if (b.price.slice(3) < a.price.slice(3)) 
+			return 1;
 
         else if (b.price.slice(3) > a.price.slice(3)) 
 			return -1;
+
+      return 0;
       });
     }
     props.updatedListHandler(updatedList);
